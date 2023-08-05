dbfeb7d72bc82407bfd85b0c9e9bc81663daf38e
Arshdeep-13 <arshdeeprooprai@gmail.com>
2023-08-05 12:46:46 +0530

    Basic Layout deployed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import React, { useState } from 'react'
+import './App.css'
 
-function App() {
+export default function App() {
+  const [input, setinput] = useState("");
+  const [result, setresult] = useState("");
+  const [loader, setloader] = useState(false);
+  const fetchData = async () => {
+    try {
+      // api url -> https://api.shrtco.de/v2/shorten?url=${type your url here}
+      setloader(true);
+      const res = await axios(`https://api.shrtco.de/v2/shorten?url=${input}`);
+      setloader(false);
+      setresult(res.data.result.full_short_link);
+    }
+    catch (err) {
+      alert(err);
+    }
+  }
+  const handleClick = () => {
+    fetchData();
+    setinput("");
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>URL Shortner Web App</h1>
+      <div className="container">
+        <div className="mb-3">
+          <label htmlFor="basic-url" className="form-label">Website URL</label>
+          <div className="input-group">
+            <span className="input-group-text" id="basic-addon3">https://</span>
+            <input type="text" aria-describedby="basic-addon3 basic-addon4" className="form-control" id="basic-url"  placeholder='Enter any Link here' value={input} onChange={(e) => setinput(e.target.value)} />            
+          </div>
+          {/* <div className="form-text" id="basic-addon4">Example help text goes outside the input group.</div> */}
+        </div>
+        <button className='btn btn-success' onClick={handleClick}>Submit</button>
+        {loader === true ? "Loading..." : <a rel="noreferrer" target='_blank' href={result}>{result}</a>}
+      </div>
     </div>
-  );
+  )
 }
-
-export default App;
